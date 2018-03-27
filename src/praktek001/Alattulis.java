1b782533d85635e7f42afc8c09f1a35223cee9af
gustimuhammadraghib <gt.muhammadraghib@gmail.com>
2018-03-27 11:44:28 +0800

    Membuat Atribut dan Behavior

--- src/praktek001/Alattulis.java
+++ src/praktek001/Alattulis.java
@@ -10,5 +10,20 @@ package praktek001;
  * @author GMR
  */
 public class Alattulis {
+    public class Pakaian {
+    String Panjang;
+    String Bahan;
+    String Isi;
+    String Tebal;
+    String Warna;
     
-}
+    void tampildata(){
+        System.out.println("Panjang   : "+Panjang);
+        System.out.println("Bahan     : "+Bahan);
+        System.out.println("Isi       : "+Isi);
+        System.out.println("Tebal     : "+Tebal);
+        System.out.println("Warna     : "+Warna);
+        System.out.println(" ");
+    }
+    }
+    }
